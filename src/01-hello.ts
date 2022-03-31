7a57d922b7e85011a9c3773a7a5d1fa7599301b3
Kevin Marquez <marquezkrodriguez24@gmail.com>
2022-03-30 21:05:52 -0600

    Inicio con typeScript, tipos de datos primitivo (Tipos diferidos)

---
+++ src/01-hello.ts
@@ -0,0 +1,2 @@
+const myName = 'Kevin Marquez';
+console.log(myName)
---
+++ src/03-typing.ts
@@ -0,0 +1,11 @@
+(() => {
+    let myProductName: string = 'Producto 1';
+    let myProductNamePrice: number = 500;
+  
+    myProductName = 'Nitendo Swicth';
+    myProductName.toLocaleLowerCase();
+    myProductNamePrice.toFixed();
+  
+    const myProductStock = 250;
+    const  myProductName2 = 'Producto 1';
+  })();
